e57b7bb6720692c703e5b83651db8fb683b54702
YosuaG <yosuajayapura@gmail.com>
2025-03-21 23:28:43 +0900

    feat: REST CRUD done

--- address-service/model/address.go
+++ address-service/model/address.go
@@ -14,3 +14,30 @@ type Address struct {
 	CreatedAt time.Time     `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time     `bson:"updated_at" json:"updated_at"`
 }
+
+// PayloadCreateAddress represents the structure of the create address request data.
+// @Description PayloadCreateAddress details.
+type PayloadCreateAddress struct {
+	Name      string `json:"name" validate:"required" example:"Home"`
+	Latitude  string `json:"latitude" validate:"required" example:"40.7128"`
+	Longitude string `json:"longitude" validate:"required" example:"-74.0060"`
+}
+
+// PayloadUpdateAddress represents the structure of the update address request data.
+// @Description PayloadUpdateAddress details.
+type PayloadUpdateAddress struct {
+	Name      string `json:"name" validate:"required" example:"Office"`
+	Latitude  string `json:"latitude" validate:"required" example:"40.7128"`
+	Longitude string `json:"longitude" validate:"required" example:"-74.0060"`
+}
+
+// ResponseAddress represents the structure address response data.
+// @Description ResponseAddress details.
+type ResponseAddress struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Latitude  string    `json:"latitude"`
+	Longitude string    `json:"longitude"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
