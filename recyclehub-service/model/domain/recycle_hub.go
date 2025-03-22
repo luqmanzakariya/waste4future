636626230848df9089c226b81b8ee1688605dc13
luqmanzakariya <luqman.zakariya@gmail.com>
2025-03-22 22:32:06 +0700

    fix(recyclehub-service): move recyclehub-service to its own folder

---
+++ recyclehub-service/model/domain/recycle_hub.go
@@ -0,0 +1,12 @@
+package domain
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// RecycleHub represents a recycle hub
+type RecycleHub struct {
+	ID          primitive.ObjectID `json:"id" bson:"_id"`
+	Name        string             `json:"name" bson:"name"`
+	Phone       string             `json:"phone" bson:"phone"`
+	AddressID   string             `json:"address_id" bson:"address_id"`
+	WasteTypeID []string           `json:"waste_type_id" bson:"waste_type_id"`
+}
