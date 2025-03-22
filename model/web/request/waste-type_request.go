2b0e0e0ffbf56f66e626ea065f5b30d2c3afda81
luqmanzakariya <48648183+luqmanzakariya@users.noreply.github.com>
2025-03-22 22:36:38 +0700

    Revert "Recyclehub service with folder"

--- model/web/request/waste-type_request.go
+++
@@ -1,6 +0,0 @@
-package request
-
-// GetWasteTypeByIDRequest represents the request payload for retrieving a waste type by ID.
-type GetWasteTypeByIDRequest struct {
-	ID string `json:"id" validate:"required"` // The ID of the waste type to retrieve
-}
