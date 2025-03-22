ae30697183664e5caf08f4e5a8b5b0d7aec68a69
nafa39 <nafatulmiskiyah@gmail.com>
2025-03-22 12:58:35 +0700

    create model

---
+++ model/domain/recycle_hub.go
@@ -0,0 +1,10 @@
+package domain
+
+// RecycleHub represents a recycle hub
+type RecycleHub struct {
+	ID          string `json:"id" bson:"_id"`
+	Name        string `json:"name" bson:"name"`
+	Phone       string `json:"phone" bson:"phone"`
+	AddressID   string `json:"address_id" bson:"address_id"`
+	WasteTypeID string `json:"waste_type_id" bson:"waste_type_id"`
+}
