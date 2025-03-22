12b267ab0dd0c0a8d8482e5b9a35258e7b234a29
luqmanzakariya <luqman.zakariya@gmail.com>
2025-03-22 22:55:19 +0700

    fix merge recyclehub-service

---
+++ recyclehub-service/model/web/request/recycle-hub_request.go
@@ -0,0 +1,17 @@
+package request
+
+// RecycleHubCreateRequest represents the request payload for creating a new recycle hub.
+type RecycleHubCreateRequest struct {
+	Name        string   `json:"name" validate:"required"`          // Name of the recycle hub
+	Phone       string   `json:"phone" validate:"required"`         // Phone number of the recycle hub
+	AddressID   string   `json:"address_id" validate:"required"`    // Address ID of the recycle hub
+	WasteTypeID []string `json:"waste_type_id" validate:"required"` // Waste type ID associated with the recycle hub
+}
+
+// RecycleHubUpdateRequest represents the request payload for updating an existing recycle hub.
+type RecycleHubUpdateRequest struct {
+	Name        string   `json:"name" validate:"required"`          // Updated name of the recycle hub
+	Phone       string   `json:"phone" validate:"required"`         // Updated phone number of the recycle hub
+	AddressID   string   `json:"address_id" validate:"required"`    // Updated address ID of the recycle hub
+	WasteTypeID []string `json:"waste_type_id" validate:"required"` // Updated waste type ID associated with the recycle hub
+}
