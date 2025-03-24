edc1fffec5d9b9b25d0c15730329213b3d0e957a
luqmanzakariya <luqman.zakariya@gmail.com>
2025-03-24 16:57:26 +0700

    feat(driver-repository): update model, repository, and usecase driver

--- operation-service/model/driver.go
+++ operation-service/model/driver.go
@@ -11,6 +11,7 @@ type DriverStatus string
 
 const DriverStatusActive DriverStatus = "active"
 const DriverStatusInactive DriverStatus = "inactive"
+const DriverStatusWorking DriverStatus = "working"
 
 type Driver struct {
 	ID           bson.ObjectID `bson:"_id,omitempty" json:"id"`
@@ -25,7 +26,26 @@ type Driver struct {
 // PayloadCreateDriver represents the structure of the create driver request data.
 // @Description PayloadCreateDriver details.
 type PayloadCreateDriver struct {
-	Name         string `json:"name" example:"driver grab"`
-	Phone        string `json:"phone" example:"081394859283"`
-	LicensePlate string `json:"license_plate" example:"B 4134 LUZ"`
+	Name         string `json:"name" example:"driver grab" validate:"required"`
+	Phone        string `json:"phone" example:"081394859283" validate:"required"`
+	LicensePlate string `json:"license_plate" example:"B 4134 LUZ" validate:"required"`
+}
+
+// PayloadUpdateDriver represents the structure of the update driver request data.
+// @Description PayloadUpdateDriver details.
+type PayloadUpdateDriver struct {
+	Name         string       `json:"name" example:"driver grab"`
+	Phone        string       `json:"phone" example:"081394859283"`
+	LicensePlate string       `json:"license_plate" example:"B 4134 LUZ"`
+	Status       DriverStatus `json:"status" example:"inactive"`
+}
+
+type ResponseDriver struct {
+	ID           string       `json:"id"`
+	Name         string       `json:"name"`
+	Phone        string       `json:"phone"`
+	LicensePlate string       `json:"license_plate"`
+	Status       DriverStatus `json:"status"`
+	CreatedAt    time.Time    `json:"created_at"`
+	UpdatedAt    time.Time    `json:"updated_at"`
 }
---
+++ operation-service/usecase/driver_usecase.go
@@ -0,0 +1,137 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"operation-service/model"
+	"operation-service/repository"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type IDriverUsecase interface {
+	Create(ctx context.Context, payload model.PayloadCreateDriver) (model.ResponseDriver, error)
+	FindAll(ctx context.Context) ([]model.Driver, error)
+	FindByID(ctx context.Context, id string) (model.ResponseDriver, error)
+	Update(ctx context.Context, id string, payload model.PayloadUpdateDriver) (model.ResponseDriver, error)
+	Delete(ctx context.Context, id string) error
+}
+
+type driverUsecase struct {
+	DriverRepo repository.IDriverRepository
+	Validate   *validator.Validate
+}
+
+func NewDriverUsecase(driverRepo repository.IDriverRepository, validate *validator.Validate) IDriverUsecase {
+	return &driverUsecase{
+		DriverRepo: driverRepo,
+		Validate:   validate,
+	}
+}
+
+func (d *driverUsecase) Create(ctx context.Context, payload model.PayloadCreateDriver) (model.ResponseDriver, error) {
+	err := d.Validate.Struct(payload)
+	if err != nil {
+		return model.ResponseDriver{}, err
+	}
+
+	driver := model.Driver{
+		Name:         payload.Name,
+		Phone:        payload.Phone,
+		LicensePlate: payload.LicensePlate,
+		Status:       model.DriverStatusActive,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+	}
+
+	res, err := d.DriverRepo.Create(ctx, driver)
+	if err != nil {
+		return model.ResponseDriver{}, err
+	}
+
+	response := model.ResponseDriver{
+		ID:           res.ID.Hex(),
+		Name:         res.Name,
+		Phone:        res.Phone,
+		LicensePlate: res.LicensePlate,
+		Status:       res.Status,
+		CreatedAt:    res.CreatedAt,
+		UpdatedAt:    res.UpdatedAt,
+	}
+
+	return response, nil
+}
+
+func (d *driverUsecase) FindAll(ctx context.Context) ([]model.Driver, error) {
+	var drivers []model.Driver
+	drivers, err := d.DriverRepo.ReadAll(ctx)
+	if err != nil {
+		return drivers, err
+	}
+
+	return drivers, nil
+}
+
+func (d *driverUsecase) FindByID(ctx context.Context, id string) (model.ResponseDriver, error) {
+	var driver model.Driver
+	driver, err := d.DriverRepo.ReadByID(ctx, id)
+	if err != nil {
+		return model.ResponseDriver{}, errors.New("driver not found")
+	}
+
+	response := model.ResponseDriver{
+		ID:           driver.ID.Hex(),
+		Name:         driver.Name,
+		Phone:        driver.Phone,
+		LicensePlate: driver.LicensePlate,
+		Status:       driver.Status,
+		CreatedAt:    driver.CreatedAt,
+		UpdatedAt:    driver.UpdatedAt,
+	}
+
+	return response, nil
+}
+
+func (d *driverUsecase) Update(ctx context.Context, id string, payload model.PayloadUpdateDriver) (model.ResponseDriver, error) {
+	err := d.Validate.Struct(payload)
+	if err != nil {
+		return model.ResponseDriver{}, err
+	}
+
+	driver, err := d.DriverRepo.ReadByID(ctx, id)
+	if err != nil {
+		return model.ResponseDriver{}, errors.New("driver not found")
+	}
+
+	payloadUpdated := model.Driver{
+		ID:           driver.ID,
+		Name:         payload.Name,
+		Phone:        payload.Phone,
+		LicensePlate: payload.LicensePlate,
+		Status:       payload.Status,
+		CreatedAt:    driver.CreatedAt,
+		UpdatedAt:    time.Now(),
+	}
+
+	updatedOrder, err := d.DriverRepo.Update(ctx, id, payloadUpdated)
+	if err != nil {
+		return model.ResponseDriver{}, errors.New("error updating driver")
+	}
+
+	response := model.ResponseDriver{
+		ID:           updatedOrder.ID.Hex(),
+		Name:         updatedOrder.Name,
+		Phone:        updatedOrder.Phone,
+		LicensePlate: updatedOrder.LicensePlate,
+		Status:       updatedOrder.Status,
+		CreatedAt:    updatedOrder.CreatedAt,
+		UpdatedAt:    updatedOrder.UpdatedAt,
+	}
+
+	return response, nil
+}
+
+func (d *driverUsecase) Delete(ctx context.Context, id string) error {
+	return d.DriverRepo.Delete(ctx, id)
+}
